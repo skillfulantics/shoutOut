18587c5a12e764e447b161f44041dcae8550ae07
mikebcbc <mikebconstantino@gmail.com>
2015-11-30 17:01:19 -0500

    Fading done & increased mic height

--- js/initiate.js
+++ js/initiate.js
@@ -1,5 +1,20 @@
+/*Custom Reusables*/
+function showSkills() {
+	$("#pitbull-cut, #eventTrigger").fadeTo("slow", 0);
+};
+
+
+/*Called Events/misc*/
+
 $( "#eventTrigger" ).click(function() {
 	if ( $("#form").is( ":hidden" ) ) {
 		$("#form").slideDown("slow");
 	}
 });
+
+if (window.outerWidth && window.outerWidth < 480) {
+
+	$("#eventTrigger").click(function() {
+		showSkills();
+	});
+}
