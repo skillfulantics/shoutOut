107d88d6b44f89512c7b8ce29a13c9a911036705
mikebcbc <mikebconstantino@gmail.com>
2015-12-02 14:09:52 -0500

    Responsive Fixes

--- js/initiate.js
+++ js/initiate.js
@@ -6,15 +6,22 @@ function showSkills() {
 
 /*Called Events/misc*/
 
+if ($(".check-size").css("float") == "right") {
+	$("#eventTrigger").click(function() {
+		showSkills();
+	});
+}
+
+
 $( "#eventTrigger" ).click(function() {
 	if ( $("#form").is( ":hidden" ) ) {
 		$("#form").slideDown("slow");
 	}
 });
 
-if (window.outerWidth && window.outerWidth < 480) {
+// if (window.outerWidth && window.outerWidth < 960) {
 
-	$("#eventTrigger").click(function() {
-		showSkills();
-	});
-}
+// 	$("#eventTrigger").click(function() {
+// 		showSkills();
+// 	});
+// }
